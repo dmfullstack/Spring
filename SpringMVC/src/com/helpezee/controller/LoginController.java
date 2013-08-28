b1d283c71dfaf90909c234e3c65a14bb281cbdf2
satyanarayana pampana <satyanarayana.pampana@cbeyond.net>
2013-08-28 20:25:31 +0530

    Spring tiles,Internalization

---
+++ SpringMVC/src/com/helpezee/controller/LoginController.java
@@ -0,0 +1,73 @@
+package com.helpezee.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.helpezee.beans.Login;
+
+
+@Controller
+@SessionAttributes
+public class LoginController {
+
+@ModelAttribute("Login")
+private Login populateForm()
+{
+Login login= new Login();
+	return login;
+}
+	
+@RequestMapping("/login") // The @RequestMapping annotation tells Spring that this Controller should process all requests beginning with /login in the URL path. That includes /login/* and /login.html.
+public ModelAndView LoadLoginPage()
+{
+String welcome="Namasthe";
+return new ModelAndView("Login","welcome",welcome);
+}
+
+@RequestMapping(method= RequestMethod.POST,value="/validateUser.html")
+public String validateUser(@ModelAttribute("Login")Login login,BindingResult error){ //This will binds the data from request to the object
+	System.out.println("username---"+login.getUsername());
+	System.out.println("password---"+login.getPassword());
+	if(login.getUsername()!=null && login.getPassword()!=null)
+	{
+		return "redirect:login.html";	
+	}
+	return "redirect:login.html";
+	
+	
+}
+
+
+}
+
+/*
+Note that we have annotated the HelloWorldController class with @Controller and @RequestMapping("/hello") 
+on line 7 and 10. When Spring scans our package, it will recognize this bean as being a Controller bean 
+for processing requests. The @RequestMapping annotation tells Spring that this Controller should process 
+all requests beginning with /hello in the URL path. That includes /hello/* and /hello.html.
+
+The helloWorld() method returns ModelAndView object. The ModelAndView object tries to resolve to a 
+view named “hello” and the data model is being passed back to the browser so we can access the data within 
+the JSP. The logical view name will resolve to "/WEB-INF/jsp/hello.jsp". 
+We will discuss this shortly how the logical name “hello” which is return in ModelAndView object is mapped 
+to path /WEB-INF/jsp/hello.jsp.
+
+The ModelAndView object also contains a message with key “message” and value “Hello World, Spring 3.0!”.
+ This is the data that we are passing to our view. Normally this will be a value object in form of java bean 
+ that will contain the data to be displayed on our view. Here we are simply passing a string.
+ 
+ We will use class org.springframework.context.support.ReloadableResourceBundleMessageSource to define the 
+ message resources.
+
+Also, note that we will provide a feature where user will be able to select language for the application.
+ This is implemented by using org.springframework.web.servlet.i18n.LocaleChangeInterceptor class. 
+ The LocaleChangeInterceptor class will intercept any changes in the locale. 
+ These changes are then saved in cookies for future request. 
+ org.springframework.web.servlet.i18n.CookieLocaleResolver class will be used to store the locale changes 
+ in cookies.
+*/
